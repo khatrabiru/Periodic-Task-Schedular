fdb9fc126522356b9f008dc4093f68e847efe27a
khatrabiru <khatrabiru@gmail.com>
2021-11-24 23:04:22 +0000

    Initial Commit

---
+++ Periodic Task Schedular/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Periodic Task Schedular
+//
+//  Created by Bir Bahadur Khatri on 24/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
